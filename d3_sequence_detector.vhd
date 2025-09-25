fc86964f00355e4c46ab34730481207aa075a230
wltwt <wltwt@proton.me>
2025-09-25 16:06:10 +0200

    Seems to work

--- d3_sequence_detector.vhd
+++ d3_sequence_detector.vhd
@@ -7,18 +7,60 @@ entity d3_sequence_detector is
 		clk: in std_logic;
 		rst: in std_logic;
 		data: in std_logic;
-		seq_found: out std_logic;
+		seq_found: out std_logic
 	);
 end entity d3_sequence_detector;	
 
 architecture rtl of d3_sequence_detector is
-	signal i: natural range 0 to MAX;
+	type state_type is (S0, S1, S2, S3, S4);
+	signal state, nx_state: state_type := S0;
 begin
-	P1: process(clk)
+
+
+	P1: process(clk, rst)
 	begin
 		if rising_edge(clk) then
-			-- implement logic
+			if rst='0' then
+				state <= S0;
+			else
+				state <= nx_state;
+			end if;
 		end if;
 	end process;
-	seq_found <= '1'; -- when state S4...
+	
+	-- detect 1011
+	P2: process(all)
+	begin
+		case state is
+			when S0 =>
+				if data='1' then
+					nx_state <= S1;
+				else
+					nx_state <= S0;
+				end if;
+			when S1 =>
+				if data='0' then
+					nx_state <= S2;
+				else
+					nx_state <= S1;
+				end if;
+			when S2 =>
+				if data='1' then
+					nx_state <= S3;
+				else
+					nx_state <= S0;
+				end if;
+			when S3 =>
+				if data='1' then
+					nx_state <= S4;
+				else
+					nx_state <= S2;
+				end if;
+			when S4 =>
+				nx_state <= S1;
+		end case;
+	end process;
+	
+	seq_found <= '1' when state = S4 else '0';
+	
 end architecture rtl;
